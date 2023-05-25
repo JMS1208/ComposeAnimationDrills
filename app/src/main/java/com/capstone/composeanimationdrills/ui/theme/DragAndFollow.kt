fc4f85d39d8a6e2bdad4574e4c2d133d07bae840
JMS1208 <wjsalstjr59@gmail.com>
2023-05-26 00:34:54 +0900

    드래그 앤 팔로우 만들기

--- app/src/main/java/com/capstone/composeanimationdrills/ui/theme/DragAndFollow.kt
+++ app/src/main/java/com/capstone/composeanimationdrills/ui/theme/DragAndFollow.kt
@@ -1,12 +1,21 @@
 package com.capstone.composeanimationdrills.ui.theme
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,13 +24,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.capstone.composeanimationdrills.extension.randomText
+import com.example.compose.MyCustomTheme
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 
 @Preview(showBackground = true)
@@ -36,21 +54,51 @@ private fun Preview() {
         mutableStateOf(false)
     }
 
+    val textState by remember {
+        mutableStateOf(String.randomText(20))
+    }
+
+    val scaleState = remember {
+        Animatable(initialValue = 1f)
+    }
+
+    LaunchedEffect(isDragging) {
+        launch {
+            scaleState.animateTo(
+                targetValue = 0.9f,
+                animationSpec = tween(
+                    durationMillis = 50
+                )
+            )
+            scaleState.animateTo(
+                targetValue = 1f,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioLowBouncy,
+                    stiffness = Spring.StiffnessHigh
+                )
+            )
+        }
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Box(
             modifier = Modifier
-                .onGloballyPositioned {
-                    position = it.positionInParent()
-                }
                 .graphicsLayer {
                     if(isDragging) {
-                        translationX = position.x
-                        translationY = position.y
+                        scaleX = scaleState.value
+                        scaleY = scaleState.value
                     }
+                    translationX = position.x
+                    translationY = position.y
+
                 }
+                .background(
+                    color = if (isDragging) Black.copy(
+                        alpha = 0.1f
+                    ) else Color.Transparent
+                )
                 .pointerInput(Unit) {
                     detectDragGesturesAfterLongPress(
                         onDragStart = {
@@ -58,7 +106,6 @@ private fun Preview() {
                         },
                         onDragCancel = {
                             isDragging = false
-
                         },
                         onDragEnd = {
                             isDragging = false
@@ -72,7 +119,7 @@ private fun Preview() {
 
         ) {
             Text(
-                text = String.randomText(20)
+                text = textState
             )
         }
 
